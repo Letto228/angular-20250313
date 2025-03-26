7070971f78bb6984d8dfc1d0df06adcf485b2d05
Иван Васильев <ivasiliev1999@gmail.com>
2025-03-26 20:35:03 +0300

    DZ-2

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core';
+import {NgFor} from '@angular/common';
+import {CardComponent} from './card/card.component';
+
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
     standalone: true,
-    imports: [],
+    imports: [CardComponent, NgFor],
     templateUrl: './products-list.component.html',
     styleUrl: './products-list.component.css',
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    productsData = productsMock;
+}

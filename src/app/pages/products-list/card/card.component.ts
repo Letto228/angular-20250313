e0b0d631ae50ef3cc2bbd592ffaa996dbccb5c87
Mari Vaypane <avemari98@gmail.com>
2025-04-03 05:57:26 +0300

    add card component

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,8 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Product} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -13,20 +13,15 @@ import {productsMock} from '../../../shared/products/products.mock';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    readonly product = input.required<Product>();
 
-    onProductBuy(event: Event) {
-        event.stopPropagation();
+    readonly addItemToCart = output<Product>();
 
-        if (!this.product) {
-            return;
-        }
-
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+    onProductBuy() {
+        this.addItemToCart.emit(this.product());
     }
 
     isStarActive(starIndex: number): boolean {
-        return !!this.product && this.product.rating >= starIndex;
+        return !!this.product && this.product().rating >= starIndex;
     }
 }

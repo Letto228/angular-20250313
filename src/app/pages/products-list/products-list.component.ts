9618e545b7711b51fa7923699c280a8af9209ccb
Mari Vaypane <avemari98@gmail.com>
2025-04-03 06:01:03 +0300

    add product list component

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,12 +1,25 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 import {CardComponent} from './card/card.component';
+import {productsMock} from '../../shared/products/products.mock';
+import {Product} from '../../shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
     standalone: true,
-    imports: [CardComponent],
+    imports: [CardComponent, MatIconModule],
     templateUrl: './products-list.component.html',
     styleUrl: './products-list.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    readonly products = signal<Product[]>(productsMock);
+
+    readonly cartItems = signal<Product[]>([]);
+
+    productToCart(product: Product) {
+        const currentCart = this.cartItems();
+
+        this.cartItems.set([...currentCart, product]);
+    }
+}

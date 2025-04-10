7758806b8658b0a1d58c6ed895ab1b94a2f32210
Letto228 <letto1101@gmail.com>
2025-04-10 20:24:21 +0300

    feat: add popup host component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import {HeaderComponent} from './core-components/header/header.component';
 import {ProductsListComponent} from './pages/products-list/products-list.component';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
 import {SidenavComponent} from './core-components/sidenav/sidenav.component';
+import {PopupHostComponent} from './core-components/popup-host/popup-host.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HeaderComponent, ProductsListComponent, SidenavComponent, MatListModule],
+    imports: [
+        HeaderComponent,
+        ProductsListComponent,
+        SidenavComponent,
+        MatListModule,
+        PopupHostComponent,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,

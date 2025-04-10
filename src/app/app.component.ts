b970d73bd6cbf603b845534de316ddea8230aacf
Letto228 <letto1101@gmail.com>
2025-04-10 20:08:44 +0300

    feat: lecture 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatListModule} from '@angular/material/list';
 import {HeaderComponent} from './core-components/header/header.component';
 import {ProductsListComponent} from './pages/products-list/products-list.component';
 import {applicationConfigMock} from './shared/application-config/application-config.mock';
@@ -7,7 +8,7 @@ import {SidenavComponent} from './core-components/sidenav/sidenav.component';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [HeaderComponent, ProductsListComponent, SidenavComponent],
+    imports: [HeaderComponent, ProductsListComponent, SidenavComponent, MatListModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,9 +16,9 @@ import {SidenavComponent} from './core-components/sidenav/sidenav.component';
 export class AppComponent {
     readonly appConfig = applicationConfigMock;
 
-    readonly isDrawerOpenedStore = signal(false);
+    // readonly isDrawerOpenedStore = signal(false);
 
-    onMenuClick() {
-        this.isDrawerOpenedStore.update(isOpened => !isOpened);
-    }
+    // onMenuClick() {
+    //     this.isDrawerOpenedStore.update(isOpened => !isOpened);
+    // }
 }
--- src/app/core-components/sidenav/sidenav.component.ts
+++ src/app/core-components/sidenav/sidenav.component.ts
@@ -1,6 +1,6 @@
-import {ChangeDetectionStrategy, Component, model} from '@angular/core';
+import {ChangeDetectionStrategy, Component, viewChild} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDrawer, MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-sidenav',
@@ -11,9 +11,34 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidenavComponent {
-    readonly isDrawerOpened = model(false);
+    // readonly naviagationTemplate = input.required<TemplateRef<unknown>>();
+    // readonly isDrawerOpened = model(false);
+
+    // readonly naviagationTemplate =
+    //     contentChild.required<TemplateRef<unknown>>('naviagationTemplate');
+
+    // @ViewChild('')
+    // matDrawer: MatDrawer | undefined;
+
+    // readonly matDrawer = viewChild.required<MatDrawer>('drawer');
+    readonly matDrawer = viewChild.required(MatDrawer);
+    // readonly matDrawerElement = viewChild.required(MatDrawer, {read: ElementRef});
+
+    // readonly drawerViewport = viewChild.required('drawerViewport', {read: ViewContainerRef});
+
+    // constructor() {
+    //     // setTimeout(() => {
+    //     //     this.drawerViewport().createEmbeddedView(this.naviagationTemplate());
+    //     // }, 1000);
+
+    //     // effect(() => {
+    //     //     this.drawerViewport().createEmbeddedView(this.naviagationTemplate());
+    //     // });
+    // }
 
     toggleDrawerOpened() {
         // ToDo
+        this.matDrawer().toggle();
+        // console.log(this.matDrawerElement().nativeElement);
     }
 }

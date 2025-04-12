7e1eae484fbd369effc4ea37888cb1245262d855
Yuriy <Yura962Bur@mail.ru>
2025-04-12 12:27:40 +0400

    add product card

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,10 +1,21 @@
 import {Component} from '@angular/core';
+import {ProductCardComponent} from './card/product-card/product-card.component';
+import {productsMock} from '../../shared/products/products.mock';
 
+/** Компонент списка товаров */
 @Component({
     selector: 'app-products-list',
     standalone: true,
-    imports: [],
+    imports: [ProductCardComponent],
     templateUrl: './products-list.component.html',
     styleUrl: './products-list.component.css',
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+    get ProductList() {
+        return productsMock;
+    }
+
+    get firstProduct() {
+        return productsMock[0];
+    }
+}

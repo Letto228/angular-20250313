9eb64cfe3a0443afb6ce1784ad4b68be63ce32b3
Letto228 <letto1101@gmail.com>
2025-04-14 23:53:39 +0300

    feat: add scroll with loading

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -2,11 +2,12 @@ import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {CardComponent} from './card/card.component';
 import {ProductsStoreService} from '../../shared/products/products-store.service';
+import {ScrollWithLoadingDirective} from '../../shared/scroll-with-loading/scroll-with-loading.directive';
 
 @Component({
     selector: 'app-products-list',
     standalone: true,
-    imports: [CardComponent, MatProgressSpinnerModule],
+    imports: [CardComponent, MatProgressSpinnerModule, ScrollWithLoadingDirective],
     templateUrl: './products-list.component.html',
     styleUrl: './products-list.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,6 +17,11 @@ export class ProductsListComponent {
 
     constructor() {
         this.productsStoreService.loadProducts();
+    }
+
+    loadNextProducts(): void {
+        // eslint-disable-next-line no-console
+        console.log('Load next products');
     }
 
     getProducts(): ReturnType<ProductsStoreService['getProducts']> {
